8062660d6c5ea34e4327cffca19d17291666974d
Bhargav Bolla <hire.bhargav@gmail.com>
2018-11-16 22:16:34 -0800

    WIP How do i store the rows that occurred in a given interval.

---
+++ src/main/java/org/bbolla/indexer/specification/TimeIndexerSpec.java
@@ -0,0 +1,26 @@
+package org.bbolla.indexer.specification;
+
+import org.joda.time.Interval;
+
+import java.util.Map;
+
+/**
+ * Indexer specs.
+ * The range is stored as a long array {startInclusive, endExclusive}
+ */
+public interface TimeIndexerSpec {
+
+    /**
+     * Get all rows in a interval.
+     * @param interval
+     * @return
+     */
+    Map<String, long[]> getAllRowsInAnInterval(Interval interval);
+
+    /**
+     * Store all rows in an interval.
+     * @param interval
+     * @param startInclusiveAndEndExclusive
+     */
+    void storeAllRowsInAnInterval(Interval interval, long[] startInclusiveAndEndExclusive);
+}
