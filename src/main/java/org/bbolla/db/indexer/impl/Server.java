8ca5db308ba2425628168406009bd8d99c3f6c39
Bhargav Bolla <hire.bhargav@gmail.com>
2018-12-26 17:19:24 -0800

    Maintenance Api's

--- src/main/java/org/bbolla/db/indexer/impl/Server.java
+++ src/main/java/org/bbolla/db/indexer/impl/Server.java
@@ -1,11 +1,22 @@
 package org.bbolla.db.indexer.impl;
 
+import com.google.common.collect.ImmutableMap;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.cluster.BaselineNode;
+import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.configuration.DataStorageConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.spi.discovery.zk.ZookeeperDiscoverySpi;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 //TODO Seperate cluster maintenance functions like cluster active. change base line topology (etc)
 //TODO ***TOP PRIORITY*** topology management topology reports.
@@ -50,8 +61,6 @@ public class Server {
 
         ignite = Ignition.start(cfg);
 
-        ignite.cluster().active(true);
-
         log.warn("Storage directory set to : {}", cfg.getDataStorageConfiguration().getDefaultDataRegionConfiguration());
     }
 
@@ -59,5 +68,80 @@ public class Server {
         return ignite;
     }
 
+    public boolean activate() {
+        ignite.cluster().active(true);
+        return true;
+    }
 
+    public boolean deactivate() {
+        ignite.cluster().active(false);
+        return true;
+    }
+
+    /**
+     * Add node to baseline topology
+     * @param nodeId
+     * @return
+     */
+    public Object addNode(String nodeId) {
+        ignite.cluster().currentBaselineTopology();
+        throw new RuntimeException("Pending Implementation");
+    }
+
+
+    public Object rebalance() {
+        Collection<ClusterNode> aliveNodes = ignite.cluster().forServers().nodes();
+        Set<String> aliveSet = ids(aliveNodes);
+        Set<String> existingSet = ids(ignite.cluster().currentBaselineTopology());
+        if(aliveSet.equals(existingSet)) {
+            return false;
+        } else {
+            Object before = nodes();
+            ignite.cluster()
+                    .setBaselineTopology(
+                            aliveNodes
+                                    .stream()
+                                    .filter(n -> !n.isClient())
+                                    .collect(Collectors.toList())
+                    );
+            Object after = nodes();
+            return ImmutableMap.of("before", before, "after", after);
+        }
+    }
+
+    private static <T extends BaselineNode> Set<String> ids(Collection<T> clusterGroup) {
+        return clusterGroup.stream().map(n -> n.consistentId().toString()).collect(Collectors.toSet());
+    }
+
+    public Object removeNode(String nodeId) {
+        throw new RuntimeException("yet to be implemented");
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static class Node {
+        String ip;
+        String id;
+        String type;
+        String hostname;
+        String order;
+    }
+
+
+    public Object nodes() {
+        return ignite.cluster().nodes().stream().map(c -> {
+            Node n = new Node();
+            n.setId(c.consistentId().toString());
+            n.setIp(c.addresses().toString());
+            n.setHostname(c.hostNames().toString());
+            n.setOrder(String.valueOf(c.order()));
+            n.setType(c.isClient()? "client": "server"); //TODO only two types for now. Review later.
+            return n;
+        }).collect(Collectors.toList());
+    }
+
+    public boolean isActive() {
+        return ignite.cluster().active();
+    }
 }
---
+++ src/main/java/org/bbolla/db/maintenance/MaintenanceRestController.java
@@ -0,0 +1,99 @@
+package org.bbolla.db.maintenance;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.bbolla.db.indexer.impl.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author bbolla on 12/26/18
+ */
+@RestController
+@RequestMapping("/maintenance/cluster")
+@Slf4j
+public class MaintenanceRestController {
+
+    @Autowired
+    private Server server;
+
+    private static final String PASSWORD_HASHED = "33F6ECB99522960D01FD1CC09DE62B49";
+
+
+    @PutMapping("/activate")
+    public ResponseEntity<Object> activateCluster(@RequestHeader("password") String pass) {
+        String hashed = digestPass(pass);
+        if (PASSWORD_HASHED.equals(hashed)) {
+            boolean result = server.activate();
+            if (result) {
+                return ResponseEntity.ok(server.nodes());
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
+            }
+        } else {
+            return ResponseEntity.badRequest().body("Secret Key Missing/ Wrong");
+        }
+    }
+
+
+    @GetMapping("/status")
+    public Boolean status() {
+        return server.isActive();
+    }
+
+    @PutMapping("/deactivate")
+    public ResponseEntity<Object> deactivateCluster(@RequestHeader("password") String pass) {
+        String hashed = digestPass(pass);
+        if (PASSWORD_HASHED.equals(hashed)) {
+            boolean result = server.deactivate();
+            if (result) {
+                return ResponseEntity.ok("deactivated");
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
+            }
+        } else {
+            return ResponseEntity.badRequest().body("Secret Key Missing/ Wrong");
+        }
+    }
+
+    @PostMapping("/node/add/{id}")
+    public ResponseEntity<Object> addNodeToCluster(@PathVariable("id") String nodeId,
+                                                   @RequestHeader("password") String pass) {
+        verify(pass);
+        return ResponseEntity.ok(server.addNode(nodeId));
+    }
+
+    @PostMapping("/node/remove/{id}")
+    public ResponseEntity<Object> removeNodeFromCluster(@PathVariable("id") String nodeId,
+                                                        @RequestHeader("password") String pass) {
+        verify(pass);
+        return ResponseEntity.ok(server.removeNode(nodeId));
+    }
+
+    //TODO rebalance from a set of consistent ids.
+
+    @PutMapping("/rebalance")
+    public ResponseEntity<Object> rebalance(@RequestHeader("password") String pass) {
+        verify(pass);
+        return ResponseEntity.ok(server.rebalance());
+    }
+
+    private void verify(String pass) {
+        String hashed = digestPass(pass);
+        if (!PASSWORD_HASHED.equals(hashed)) {
+            throw new RuntimeException("Password is missing; Throw a custom exception;" +
+                    " Implement an exception advisor; since password will be used every where.");
+        }
+    }
+
+    private static String digestPass(String pass) {
+        return DigestUtils.md5Hex(pass).toUpperCase();
+    }
+
+
+    public static void main(String[] args) {
+        log.info("hashed password : {}", digestPass("something goes here"));
+    }
+}
